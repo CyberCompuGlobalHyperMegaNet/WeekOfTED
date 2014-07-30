0b1730dbf4f277e55e611e4dbbda6103de4aad3a
katbuilt <audiocassette@gmail.com>
2014-07-30 15:08:06 -0400

    install and set up Grunt with compilers and watch tasks; dev and dist folders

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+      // sass files get compiled into css files
+     sass: {
+      dist: {
+        files: {    //where files go to or from
+      'dist/main.css': 'dev/main.scss'   // destination: source
+      }
+      }
+     },
+     // Jade compiler
+     jade: {
+       compile: {
+           files: [{
+              src: "**/*.jade",
+              dest: "dist/html/",  // output jade files to an html folder
+              ext: ".html",
+              expand: true,  
+              cwd: "dev/jade/" // grab jade files from a folder called jade
+           }]
+       }
+     },
+    // JShint
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['css/*.scss', '<%= jshint.files %>'],
+        tasks: ['sass', 'jshint', 'autoprefixer']
+      },
+       jade: {
+                files: ['jade/*.jade'], // watch jade files in jade folder
+                tasks: ['jade']
+       },
+       options: {
+          livereload: true
+       }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 5 version', 'ie 7', 'ie 8', 'ie 9']
+      },
+      no_dest: {
+        src: 'css/main.css'
+      }
+    },
+    connect: {
+        server: {
+          options: {
+            port: 9001,
+            base: ''
+          }
+        }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('prefixr', ['autoprefixer']);
+
+  grunt.registerTask('default', ['jshint', 'sass','jade','connect','watch']);
+
+};
